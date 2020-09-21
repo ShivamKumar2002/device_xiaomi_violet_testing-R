149f0c7d7d8effd8129691b17d7204a5a84c7513
SayanAcharya <rowdysayan99@gmail.com>
2020-09-21 11:44:24 -0300

    Build vendor.qti.hardware.btconfigstore@2.0
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
    Change-Id: Ia11814a9907a1bd70dc7342da2f54cc23979aa91
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- device.mk
+++ device.mk
@@ -133,7 +133,10 @@ PRODUCT_PACKAGES += \
     liba2dpoffload \
     libbthost_if \
     libldacBT_dec \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0 \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Boot SPL
 BOOT_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
