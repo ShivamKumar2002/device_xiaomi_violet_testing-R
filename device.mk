9dab234285547660860424b25fbaa2c47843b49d
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2020-10-12 11:24:13 +0000

    Uprev audio and audio effect to 6.0
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- device.mk
+++ device.mk
@@ -90,9 +90,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
